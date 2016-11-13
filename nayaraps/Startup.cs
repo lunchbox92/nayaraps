395c3767510afd0e0a3769823b780071fa8a2ffc
Mark Miller <Mark Miller@DESKTOP-Q3UJFNE>
2016-11-13 12:19:32 -0500

    Add project files.

---
+++ nayaraps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nayaraps.Startup))]
+namespace nayaraps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
